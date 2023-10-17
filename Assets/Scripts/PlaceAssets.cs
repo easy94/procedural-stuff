1e2612e047eb404c36919a1dd1591cc23e871ac9
easy94 <127341824+easy94@users.noreply.github.com>
2023-10-17 08:01:10 +0200

    raycast not done
    not done

--- Assets/Scripts/PlaceAssets.cs
+++ Assets/Scripts/PlaceAssets.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class PlaceAssets
@@ -9,12 +10,22 @@ public static class PlaceAssets
     public static void CastRayOnTerrain(OozeType ooze) //GetOozedPositions param
     {
         System.Random rand = new System.Random();
+        Ray ray = new Ray();
+        ray.direction = Vector3.down;
+        RaycastHit hit = new RaycastHit();
 
-        
+        foreach (var item in ooze.GetOozedPositions())
+        {
+            Physics.Raycast(item,Vector3.down,out hit);
+            if (IsNotSteep(hit))
+            {
+
+            }
+        }
 
     }
 
-    public static bool CheckSteepness()
+    public static bool IsNotSteep(RaycastHit x)
     {
 
 
